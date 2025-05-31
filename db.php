01586144602ff5287d91592340f4177214d3511e
THAMILSELLVAN A/L PAMANATHE <tamilselvaan2002@gmail.com>
2025-05-31 19:40:43 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,11 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "123456";
+$host = "db-mysql-nyc3-88224-do-user-22958682-0.l.db.ondigitalocean.com";
+$user = "doadmin";
+$pass = "AVNS_uLb7iZGvlPLSuxlXYUW";
 $dbname = "iemsis";
+$port = "25060";
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+$conn = new mysqli($host, $user, $pass, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
