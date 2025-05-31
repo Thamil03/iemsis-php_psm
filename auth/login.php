373c0afda8f68cc8bdb3c6947b4a5885828eeee9
haru0l <tamilselvaan2002@gmail.com>
2025-06-01 00:35:30 +0800

    Update login.php

--- auth/login.php
+++ auth/login.php
@@ -1,13 +1,6 @@
 <?php
 session_start();
 
-// ✅ CORS headers
-header("Access-Control-Allow-Origin: *");
-//header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Content-Type: application/json");
-
 // ✅ Include DB connection
 include('../db.php');
 
